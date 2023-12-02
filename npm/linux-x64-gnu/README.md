e42a193f5521f7efaaa4c328a4c1f83540a0a2dc
hanyun <1355081829@qq.com>
2023-12-02 22:52:17 +0800

    Init

---
+++ npm/linux-x64-gnu/README.md
@@ -0,0 +1,3 @@
+# `carp-fibonacci-linux-x64-gnu`
+
+This is the **x86_64-unknown-linux-gnu** binary for `carp-fibonacci`
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![deny(clippy::all)]
+
+#[macro_use]
+extern crate napi_derive;
+
+#[napi]
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[napi]
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        1 | 2 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
